1434c1ba57e0360698b2ab9ce9d10c74a50d1df1
YASHASVASISHTA <112754882+YASHASVASISHTA@users.noreply.github.com>
2023-05-01 13:07:38 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h1>yashs</h1>
+        <p>loremdfssddsfdsf vdssddsf
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
